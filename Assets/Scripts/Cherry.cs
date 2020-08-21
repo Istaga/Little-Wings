5f23ee7623e45e6ac0d5f99790652c0a5c8630d8
istaga <imadethislastnight@gmail.com>
2020-08-20 20:52:46 -0400

    added cherry breathing effect

---
+++ Assets/Scripts/Cherry.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cherry : MonoBehaviour
+{
+    private float time = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time % 1 > 0.5){
+            // increase
+            transform.localScale += new Vector3(0.00035f, 0.00035f, 0);
+            Debug.Log(1.4f%2);
+        }
+        else {
+            // decrease
+            transform.localScale -= new Vector3(0.00035f, 0.00035f, 0);
+        }
+
+
+        time += Time.deltaTime;
+    }
+}
