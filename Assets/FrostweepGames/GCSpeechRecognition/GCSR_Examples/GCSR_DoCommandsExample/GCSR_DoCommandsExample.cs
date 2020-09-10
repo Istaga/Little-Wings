938d01a3585adc268ce89e1ad99206e8ba56863c
istaga <imadethislastnight@gmail.com>
2020-09-10 12:18:06 -0400

    GCSR compiles

--- Assets/FrostweepGames/GCSpeechRecognition/GCSR_Examples/GCSR_DoCommandsExample/GCSR_DoCommandsExample.cs
+++ Assets/FrostweepGames/GCSpeechRecognition/GCSR_Examples/GCSR_DoCommandsExample/GCSR_DoCommandsExample.cs
@@ -14,286 +14,128 @@ namespace FrostweepGames.Plugins.GoogleCloud.SpeechRecognition.Examples
 
 	{
 
-		public GameObject piwi;
-
-
-
+		[SerializeField] 
 		private GCSpeechRecognition _speechRecognition;
 
+		[SerializeField]
+		private Button _startRecord, _stopRecord, _recognize;
 
+		[SerializeField]
+		private Dropdown _language;
+		
+		[SerializeField]
+		private Text _result;
+		
+		private void Start(){
 
-		private Button _startRecordButton,
+			_startRecord.onClick.AddListener(StartRecordButtonOnClickHandler);
+			_stopRecord.onClick.AddListener(StopRecordButtonOnClickHandler);
+			_recognize.onClick.AddListener(RecognizeButtonOnClickHandler);
 
-					   _stopRecordButton;
+			_language.ClearOptions();
 
+			for (int i = 0; i< Enum.GetNames(typeof(Enumerators.LanguageCode)).Length; i++){
+				_language.options.Add(new Dropdown.OptionData(((Enumerators.LanguageCode)i).Parse()));
+			}
 
+			_language.value = _language.options.IndexOf(_language.options.Find(x => x.text == Enumerators.LanguageCode.en_GB.Parse()));
 
-		private Dropdown _languageDropdown;
-
-
-
-		private void Start()
-
-		{
-
-			piwi = GameObject.Find("Kiwi");
-
-			_speechRecognition = GCSpeechRecognition.Instance;
-
+			// try 1 c if fails
 			_speechRecognition.RecognizeSuccessEvent += RecognizeSuccessEventHandler;
-
-
-
-			_speechRecognition.FinishedRecordEvent += FinishedRecordEventHandler;
-
+			_speechRecognition.RecognizeFailedEvent += RecognizeFailedEventHandler;
+			
+			_speechRecognition.StartedRecordEvent += StartedRecordEventHandler;
 			_speechRecognition.RecordFailedEvent += RecordFailedEventHandler;
 
-
-
 			_speechRecognition.EndTalkigEvent += EndTalkigEventHandler;
-
-
-
-			_startRecordButton = transform.Find("Canvas/Button_StartRecord").GetComponent<Button>();
-
-			_stopRecordButton = transform.Find("Canvas/Button_StopRecord").GetComponent<Button>();
-
-
-
-			_languageDropdown = transform.Find("Canvas/Dropdown_Language").GetComponent<Dropdown>();
-
-
-
-			_startRecordButton.onClick.AddListener(StartRecordButtonOnClickHandler);
-
-			_stopRecordButton.onClick.AddListener(StopRecordButtonOnClickHandler);
-
-
-
-			_startRecordButton.interactable = true;
-
-			_stopRecordButton.interactable = false;
-
-
-
-			_languageDropdown.ClearOptions();
-
-
-
-			_speechRecognition.RequestMicrophonePermission(null);
-
-
-
-			for (int i = 0; i < Enum.GetNames(typeof(Enumerators.LanguageCode)).Length; i++)
-
-			{
-
-				_languageDropdown.options.Add(new Dropdown.OptionData(((Enumerators.LanguageCode)i).Parse()));
-
-			}
-
-
-
-			_languageDropdown.value = _languageDropdown.options.IndexOf(_languageDropdown.options.Find(x => x.text == Enumerators.LanguageCode.en_GB.Parse()));
-
-
-
-
-
-			// select first microphone device
-
-			if (_speechRecognition.HasConnectedMicrophoneDevices())
-
-			{
-
-				_speechRecognition.SetMicrophoneDevice(_speechRecognition.GetMicrophoneDevices()[0]);
-
-			}
-
 		}
 
-
-
-		private void OnDestroy()
-
-		{
-
+		private void OnDestroy(){
 			_speechRecognition.RecognizeSuccessEvent -= RecognizeSuccessEventHandler;
-
-
-
-			_speechRecognition.FinishedRecordEvent -= FinishedRecordEventHandler;
-
+			_speechRecognition.RecognizeFailedEvent -= RecognizeFailedEventHandler;
+			
+			_speechRecognition.StartedRecordEvent -= StartedRecordEventHandler;
 			_speechRecognition.RecordFailedEvent -= RecordFailedEventHandler;
 
-
-
 			_speechRecognition.EndTalkigEvent -= EndTalkigEventHandler;
-
 		}
 
-
-
-		private void StartRecordButtonOnClickHandler()
-
-		{
-
-			_startRecordButton.interactable = false;
-
-			_stopRecordButton.interactable = true;
-
-
-
+		private void StartRecordButtonOnClickHandler(){
+			_startRecord.interactable = false;
+			_stopRecord.interactable = true;
+			_result.text = string.Empty;
 			_speechRecognition.StartRecord(false);
-
 		}
 
-
-
-		private void StopRecordButtonOnClickHandler()
-
-		{
-
-			_stopRecordButton.interactable = false;
-
-			_startRecordButton.interactable = true;
-
-
-
+		private void StopRecordButtonOnClickHandler(){
+			_stopRecord.interactable = false;
+			_startRecord.interactable = true;
 			_speechRecognition.StopRecord();
-
 		}
 
-
-
-
-
-		private void RecordFailedEventHandler()
-
-		{
-
-			_stopRecordButton.interactable = false;
-
-			_startRecordButton.interactable = true;
-
-		}
-
-
-
-		private void EndTalkigEventHandler(AudioClip clip, float[] raw)
-
-		{
-
-			FinishedRecordEventHandler(clip, raw);
-
-		}
-
-
-
-		private void FinishedRecordEventHandler(AudioClip clip, float[] raw)
-
-		{
-
-
-
-			if (clip == null)
-
+		private void RecognizeButtonOnClickHandler(){
+			if(_speechRecognition.LastRecordedClip == null || _speechRecognition.LastRecordedRaw == null){
 				return;
-
-
-
+			}
 			RecognitionConfig config = RecognitionConfig.GetDefault();
-
-			config.languageCode = ((Enumerators.LanguageCode)_languageDropdown.value).Parse();
-
-			config.audioChannelCount = clip.channels;
-
-			// configure other parameters of the config if need
-
-
+			// try cap L
+			config.languageCode = ((Enumerators.LanguageCode)_language.value).Parse();
+			// config.speechContexts = new SpeechContext[]
+			// {
+			// 	new SpeechContext()
+			// 	{
+			// 		phrases = _contextPhrasesInputField.text.Replace(" ", string.Empty).Split(',')
+			// 	}
+			// };
+			config.audioChannelCount = _speechRecognition.LastRecordedClip.channels;
 
 			GeneralRecognitionRequest recognitionRequest = new GeneralRecognitionRequest()
-
 			{
-
 				audio = new RecognitionAudioContent()
-
 				{
-
-					content = raw.ToBase64()
-
+					content = _speechRecognition.LastRecordedRaw.ToBase64()
 				},
-
-				//audio = new RecognitionAudioUri() // for Google Cloud Storage object
-
-				//{
-
-				//	uri = "gs://bucketName/object_name"
-
-				//},
-
 				config = config
-
 			};
 
-
-
 			_speechRecognition.Recognize(recognitionRequest);
+		}
 
+		private void RecognizeSuccessEventHandler(RecognitionResponse recogitionResponse){
+			_result.text = "Recognize success.";
+
+			if (recogitionResponse == null || recogitionResponse.results.Length == 0){
+				_result.text = "\nWords not detected";
+				return;
+			}
+
+			_result.text += "\n" + recogitionResponse.results[0].alternatives[0].transcript;
+		}
+
+		private void StartedRecordEventHandler(){
+			_result.text = "StartedRecordEventHandler";
+		}
+
+		
+
+		private void RecognizeFailedEventHandler(string error){
+			_result.text = "Recognition failed, error: " + error;
 		}
 
 
+		private void RecordFailedEventHandler(){
+			_result.text = "RecordFailedHandler";
+			_stopRecord.interactable = false;
+			_startRecord.interactable = true;
+		}
 
-		private void RecognizeSuccessEventHandler(RecognitionResponse res)
-
-		{
-
-			string w = res.results[0].alternatives[0].words[0].word;
-
-			DoCommand(w);
-
-
-
-			// foreach (var result in res.results)
-
-			// {
-
-			// 	foreach (var alternative in result.alternatives)
-
-			// 	{
-
-			// 		foreach (var command in commands)
-
-			// 		{
-
-			// 			if (command.Trim(' ').ToLowerInvariant() == alternative.transcript.Trim(' ').ToLowerInvariant())
-
-			// 			{
-
-			// 				DoCommand(command.ToLowerInvariant().TrimEnd(' ').TrimStart(' '));
-
-			// 			}
-
-			// 		}
-
-			// 	}
-
-			// }
-
+		private void EndTalkigEventHandler(AudioClip clip, float[] raw){
+			_result.text = "EndTalkigEventHandler";
+			//FinishedRecordEventHandler(clip, raw);
 		}
 
 
-
-		private void DoCommand(string req)
-
-		{
-
-			Debug.Log(req);
-
-			Debug.Log(piwi.transform);
-
-		}
-
+	
 	}
 
 }
